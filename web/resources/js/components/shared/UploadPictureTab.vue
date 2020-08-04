fb7a79b7a615577a3c69ef5801ea5fcb64b7348f
kyokyokyo0217 <kyotaroishikawa@gmail.com>
2020-08-04 20:21:27 +0900

    fixed width and heights issue in UploadPictureTab.vue

--- web/resources/js/components/shared/UploadPictureTab.vue
+++ web/resources/js/components/shared/UploadPictureTab.vue
@@ -3,29 +3,17 @@
     <v-container>
       <v-row>
         <v-col cols="8">
-          <validation-errors-alert v-if="errors" :errors=errors.title></validation-errors-alert>
-          <v-text-field
-            v-model="title"
-            label="Title"
-            required
-            color="black"
-          ></v-text-field>
-          <validation-errors-alert v-if="errors" :errors=errors.picture></validation-errors-alert>
-          <v-file-input
-            label="File Input"
-            @change="onFileChange"
-            id="picture"
-            color="black"
-          >
-          </v-file-input>
+          <validation-errors-alert v-if="errors" :errors="errors.title"></validation-errors-alert>
+          <v-text-field v-model="title" label="Title" required color="black"></v-text-field>
+          <validation-errors-alert v-if="errors" :errors="errors.picture"></validation-errors-alert>
+          <v-file-input label="File Input" @change="onFileChange" id="picture" color="black"></v-file-input>
           <v-col class="text-right">
             <v-btn color="black" outlined @click="uploadFile" :loading="loading">submit</v-btn>
           </v-col>
         </v-col>
         <v-col cols="4">
-          <v-card color="grey" min-height="300px" min-width="300px">
-            <v-img :src="preview" v-if="preview" width="300px" height="300px" min-height="300px" min-width="300px">
-            </v-img>
+          <v-card color="grey" width="300px" height="300px">
+            <v-img :src="preview" v-if="preview" width="300px" height="300px"></v-img>
           </v-card>
         </v-col>
       </v-row>
@@ -33,70 +21,68 @@
   </v-form>
 </template>
 <script>
-import { CREATED, UNPROCESSABLE_ENTITY } from '../../util'
-import ValidationErrorsAlert from '../shared/ValidationErrorsAlert.vue'
+import { CREATED, UNPROCESSABLE_ENTITY } from "../../util";
+import ValidationErrorsAlert from "../shared/ValidationErrorsAlert.vue";
 export default {
   components: {
     ValidationErrorsAlert
   },
-  data: function () {
+  data: function() {
     return {
-      title: '',
+      title: "",
       preview: null,
       file: null,
       errors: null,
       loading: false
-    }
+    };
   },
   methods: {
     // ファイルが１つの前提
-    onFileChange (event) {
-
-      if(!event){
-        this.reset()
-        return false
+    onFileChange(event) {
+      if (!event) {
+        this.reset();
+        return false;
       }
 
-      const reader = new FileReader()
+      const reader = new FileReader();
 
       reader.onload = e => {
-        this.preview = e.target.result
-      }
+        this.preview = e.target.result;
+      };
 
-      reader.readAsDataURL(event)
+      reader.readAsDataURL(event);
 
-      this.file = event
+      this.file = event;
     },
-    reset () {
-      this.preview = ''
-      this.file = null
-      this.$el.querySelector('#picture').value = null
-
+    reset() {
+      this.preview = "";
+      this.file = null;
+      this.$el.querySelector("#picture").value = null;
     },
-    async uploadFile(){
-      this.loading = true
-      const formData = new FormData()
-      formData.append('picture', this.file)
-      formData.append('title', this.title)
-      const response = await axios.post('/api/pictures', formData)
+    async uploadFile() {
+      this.loading = true;
+      const formData = new FormData();
+      formData.append("picture", this.file);
+      formData.append("title", this.title);
+      const response = await axios.post("/api/pictures", formData);
 
       if (response.status === UNPROCESSABLE_ENTITY) {
-        this.errors = response.data.errors
-        this.loading = false
-        return false
+        this.errors = response.data.errors;
+        this.loading = false;
+        return false;
       }
 
-      this.reset()
+      this.reset();
 
       if (response.status !== CREATED) {
-        this.$store.commit('error/setCode', response.status)
-        return false
+        this.$store.commit("error/setCode", response.status);
+        return false;
       }
 
-      this.loading = false
+      this.loading = false;
 
-      this.$router.push('/explore')
+      this.$router.push("/explore");
     }
   }
-}
+};
 </script>
