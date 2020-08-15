177f5d313c8a567bc24c9bde6d0c712a74dbe022
kyokyokyo0217 <kyotaroishikawa@gmail.com>
2020-08-15 15:49:28 +0900

    fix WorksIndex.vue

--- web/resources/js/components/shared/WorksIndex.vue
+++ web/resources/js/components/shared/WorksIndex.vue
@@ -1,24 +1,21 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col
-        v-if="selectedChip == 'music'"
-        v-for="(track, index) in tracks"
-        :key="index"
-        class="d-flex child-flex"
-        :cols="cols"
-      >
-        <track-card :item="track" :edit="edit" @fetchTracks="fetchTracks"></track-card>
-      </v-col>
-      <v-col
-        v-if="selectedChip == 'picture'"
-        v-for="(picture, index) in pictures"
-        :key="index"
-        class="d-flex child-flex"
-        :cols="cols"
-      >
-        <picture-card :item="picture" :edit="edit" @fetchPhotos="fetchPhotos"></picture-card>
-      </v-col>
+      <template v-if="selectedChip === 'music'">
+        <v-col v-for="(track, index) in tracks" :key="index" class="d-flex child-flex" :cols="cols">
+          <track-card :item="track" :edit="edit" @fetchTracks="fetchTracks"></track-card>
+        </v-col>
+      </template>
+      <template v-if="selectedChip === 'picture'">
+        <v-col
+          v-for="(picture, index) in pictures"
+          :key="index"
+          class="d-flex child-flex"
+          :cols="cols"
+        >
+          <picture-card :item="picture" :edit="edit" @fetchPhotos="fetchPhotos"></picture-card>
+        </v-col>
+      </template>
     </v-row>
   </v-container>
 </template>
